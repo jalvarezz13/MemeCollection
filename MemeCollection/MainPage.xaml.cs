d694d233650b9d32f973dfa8af18f57dbcede719
Juan <Juan.Munoz7@alu.uclm.es>
2021-04-29 20:17:14 +0200

    Creacion de clases para la navegacion de la aplicacion.

--- MemeCollection/MainPage.xaml.cs
+++ MemeCollection/MainPage.xaml.cs
@@ -25,6 +25,43 @@ namespace MemeCollection
         public MainPage()
         {
             this.InitializeComponent();
+            Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(320, 320));
+            Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().VisibleBoundsChanged += MainPage_VisibleBoundsChanged;
+        }
+
+        private void MainPage_VisibleBoundsChanged(Windows.UI.ViewManagement.ApplicationView sender, object args)
+        {
+            var Width = Windows.UI.ViewManagement.ApplicationView.GetForCurrentView().VisibleBounds.Width;
+            if (Width >= 720)
+            {
+                svMenu.IsPaneOpen = true;
+                svMenu.DisplayMode = SplitViewDisplayMode.CompactInline;
+            }
+            else if (Width >= 360)
+            {
+                svMenu.IsPaneOpen = false;
+                svMenu.DisplayMode = SplitViewDisplayMode.CompactOverlay;
+            }
+            else
+            {
+                svMenu.IsPaneOpen = false;
+                svMenu.DisplayMode = SplitViewDisplayMode.Overlay;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            svMenu.IsPaneOpen = !svMenu.IsPaneOpen;
+        }
+
+        private void irRecientes(object sender, PointerRoutedEventArgs e)
+        {
+            frmMain.Navigate(typeof(RecientesPage));
+        }
+
+        private void irCategorias(object sender, PointerRoutedEventArgs e)
+        {
+            frmMain.Navigate(typeof(CategoriasPage));
         }
     }
 }
